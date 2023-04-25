b49efd224a11b818a48fb273d367cdddf425ce87
Andryuha <astik452@gmail.com>
2023-04-26 01:28:40 +0300

    Added an additinal field to json-file to hold the model's state

--- loadJson.py
+++ loadJson.py
@@ -7,7 +7,7 @@ from json import load, dump
 labels = {}
 directories = {}
 
-
+# Загружает найденные каталоги в json файл
 def loadDirs():
     global labels, directories
 
@@ -51,25 +51,44 @@ def loadDirs():
     with open(pathJoin(baseDir, "mainInfo.json"), "w") as f:
         dump(a, f, ensure_ascii=False, indent=4)
 
+# Возвращает состояние модели нейронной сети
+def getModelState():
+    # Открывает json файл
+    with open(pathJoin(directories["base"], "mainInfo.json"), "r") as f:
+        # И возвращает нужное значение
+        return load(f)["model_is_ok"]
 
-def dump_parameters(params=None):
+# Устанавливает состояние модели нейронной сети
+def setModelState(state):
+    # Открывает файл для чтения
+    with open(pathJoin(directories["base"], "mainInfo.json"), "r") as f:
+        # Записывает в локальный словарь необходимое значение
+        # Состояния модели
+        a = load(f)
+        a["model_is_ok"] = state
+
+    # Открывает файл снова и перезаписывает json файл
+    with open(pathJoin(directories["base"], "mainInfo.json"), "w") as f:
+        dump(a, f, ensure_ascii=False, indent=4)
+
+def dumpParameters(params=None):
     if "model" not in directories or \
             params is None:
         params = [0, 0, 0]
 
     v = list(map(int, params))
 
-    with open(pathJoin(directories["model"], "mainInfo.json"), "r") as f:
+    with open(pathJoin(directories["base"], "mainInfo.json"), "r") as f:
         a = load(f)
         a["parameters"] = {"left": v[0], "right": v[1], "seat": v[2]}
 
-    with open(pathJoin(directories["model"], "mainInfo.json"), "w") as f:
+    with open(pathJoin(directories["base"], "mainInfo.json"), "w") as f:
         dump(a, f, ensure_ascii=False, indent=4)
 
 
-def pull_parameters():
+def pullParameters():
     if "model" in directories:
-        with open(pathJoin(directories["model"], "mainInfo.json"), "r") as f:
+        with open(pathJoin(directories["base"], "mainInfo.json"), "r") as f:
             return list(load(f)["parameters"].values())
     else:
         return [0, 0, 0]

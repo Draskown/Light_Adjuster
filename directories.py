82eda40f5916650e7b85078e90ec252bc09241f5
Andryuha <astik452@gmail.com>
2023-04-27 01:01:54 +0300

    Renamed the folder of images

--- directories.py
+++ directories.py
@@ -6,7 +6,7 @@ class Directories():
     # Инициализация нужных для работы каталогов
     base_dir = dirname(abspath(__file__))
     model_dir = join_paths(base_dir, "Model")
-    images_dir = join_paths(base_dir, "Images")
+    images_dir = join_paths(base_dir, "Custom NN Images")
     train_dir = join_paths(images_dir, "Train")
     val_dir = join_paths(images_dir, "Validation")
     test_dir = join_paths(images_dir, "Test")
